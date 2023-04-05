ce9a1d3ec6eb003f5270cccb597a91c797cfeea2
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2023-04-05 15:37:53 -0700

    Finish OpenAI API integration

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -4,12 +4,24 @@ import { Link, useNavigate } from "react-router-dom"
 import CreatableReactSelect from "react-select/creatable"
 import { NoteData, Tag } from "./App"
 import { v4 as uuidV4 } from "uuid"
+import { Configuration, OpenAIApi } from "openai";
+
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void
   onAddTag: (tag: Tag) => void
   availableTags: Tag[]
 } & Partial<NoteData>
+
+type OptionObjectType = {
+  model: string,
+  temperature: number,
+  max_tokens: number,
+  top_p: number,
+  frequency_penalty: number,
+  presence_penalty: number,
+  prompt: string,
+}
 
 export function NoteForm({
   onSubmit,
@@ -19,6 +31,11 @@ export function NoteForm({
   markdown = "",
   tags = [],
 }: NoteFormProps) {
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+
   const titleRef = useRef<HTMLInputElement>(null)
   const markdownRef = useRef<HTMLTextAreaElement>(null)
   const [selectedTags, setSelectedTags] = useState<Tag[]>(tags)
@@ -36,15 +53,40 @@ export function NoteForm({
     navigate("..")
   }
 
+  async function handleChatGPTQuery(e: FormEvent) {
+    e.preventDefault()
+
+    const query = titleRef.current!.value
+
+    const optionObject: OptionObjectType = {
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0, 
+      prompt: query,
+    };
+
+    const response = await openai.createCompletion(optionObject)
+
+    markdownRef.current!.value = response.data.choices[0].text as string
+  }
+
   return (
     <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
+          <Stack gap={2}>
             <Form.Group controlId="title">
-              <Form.Label className="main-page-texts">Title</Form.Label>
-              <Form.Control ref={titleRef} required defaultValue={title} />
-            </Form.Group>
+                <Form.Label className="main-page-texts">Title</Form.Label>
+                <Form.Control ref={titleRef} required defaultValue={title} />
+              </Form.Group>
+              <Button type="submit" variant="warning" onClick={handleChatGPTQuery}>
+                Ask ChatGPT
+              </Button>
+          </Stack>
           </Col>
           <Col>
             <Form.Group controlId="tags">
